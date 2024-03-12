699f5e5f2044f859dada25b74b3107b7d850259a
garobcsi <gasparrobert0908@gmail.com>
2024-03-12 20:50:03 +0100

    Updated UnitTest

--- main_test.cpp
+++ main_test.cpp
@@ -3,13 +3,7 @@
 
 int main() {
     TEST(Test, test1) {
-        EXPECT_EQ(true, false);
-    } END
-    TEST(Test, test2) {
         EXPECT_EQ(true, true);
-    } END
-    TEST(Test, test3) {
-        EXPECT_EQ(true, false);
     } END
 
     return gtest_lite::Test::getTest().failed >=1;
