179dbee40d0130a6fd7b0d58585a6707f11f5488
garobcsi <gasparrobert0908@gmail.com>
2024-04-20 18:09:50 +0200

    added contacts docs

--- src/phonebook/contacts.h
+++ src/phonebook/contacts.h
@@ -9,12 +9,37 @@ class Contacts : public Array<Contact>
 {
 
 public:
+    /**
+     * @brief Alapértelmezett konstruktor létrehoz egy üres kapcsolatok listát.
+     */
     Contacts();
+
+    /**
+     * @brief Másoló konstruktor, ami létrehoz egy új kapcsolatok listát az adott listából.
+     * 
+     * @param a A másolandó kapcsolatok lista.
+     */
     Contacts(const Contacts &a);
+
+    /**
+     * @brief Konstruktor, ami létrehoz egy kapcsolatok listát az initializer_list-ből.
+     * 
+     * @param list Az initializer_list, amiből létrehozzuk a kapcsolatok listáját.
+     */
     Contacts(std::initializer_list<Contact> list);
 
+    /**
+     * @brief Betölti a kapcsolatokat egy fájlból.
+     * 
+     * @param fileName A betöltendő fájl elérési útja.
+     */
     void loadFile(const String &fileName);
 
+    /**
+     * @brief Menti a kapcsolatokat egy fájlba.
+     * 
+     * @param fileName A mentendő fájl elérési útja.
+     */
     void saveFile(const String &fileName);
 };
 
