b5f01b0b4ddb4d96afc6ccf9ef5825cd836a6841
garobcsi <gasparrobert0908@gmail.com>
2024-04-25 21:08:43 +0200

    updated clear

--- menu.cpp
+++ menu.cpp
@@ -26,10 +26,10 @@ int Menu::Render() {
 
 void Menu::Clear() {
 #if defined _WIN32
-    system("cls");
+    int err = system("cls");
 #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-    system("clear");
+    int err = system("clear");
 #elif defined (__APPLE__)
-    system("clear");
+    int err = system("clear");
 #endif
 }
